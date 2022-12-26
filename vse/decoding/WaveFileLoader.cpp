55a183a90fcd2149be0f7679f2557ac343820058
ttsuki <ttsuki@ttsuki.dev>
2022-12-26 19:19:29 +0900

    Update WaveFileLoading interface

--- vse/decoding/WaveFileLoader.cpp
+++ vse/decoding/WaveFileLoader.cpp
@@ -155,35 +155,90 @@ namespace vse
 
 namespace vse
 {
-    std::shared_ptr<ISeekableWaveSource> ReadOutToMemory(std::shared_ptr<IWaveSource> input)
+    std::shared_ptr<IRandomAccessWaveBuffer> AllocateWaveBuffer(PcmWaveFormat format)
+    {
+        class RandomAccessWaveBufferImpl : public IRandomAccessWaveBuffer
+        {
+            PcmWaveFormat format_{};
+            xtl::random_access_memory_stream ms_{};
+
+        public:
+            RandomAccessWaveBufferImpl(const PcmWaveFormat& format) : format_(format) {}
+            [[nodiscard]] PcmWaveFormat GetFormat() const override { return format_; }
+            [[nodiscard]] size_t Read(void* buffer, size_t cursor, size_t length) const noexcept override { return ms_.read(buffer, cursor, length); }
+            [[nodiscard]] size_t Write(const void* buffer, size_t cursor, size_t length) noexcept override { return ms_.write(buffer, cursor, length); }
+            [[nodiscard]] size_t Size() const override { return ms_.size(); }
+            [[nodiscard]] size_t Resize(size_t length) override { return ms_.resize(length); }
+        };
+
+        return std::make_shared<RandomAccessWaveBufferImpl>(format);
+    }
+
+    std::shared_ptr<IRandomAccessWaveBuffer> ReadOutToMemory(std::shared_ptr<IWaveSource> input)
     {
         if (!input) return nullptr;
 
-        class XtlMemStreamWaveSourceImpl : public ISeekableWaveSource
-        {
-            PcmWaveFormat format_{};
-            xtl::memory_stream ms_{};
-        public:
-            XtlMemStreamWaveSourceImpl(const PcmWaveFormat& format) : format_(format) {}
-            [[nodiscard]] PcmWaveFormat GetFormat() const override { return format_; }
-            [[nodiscard]] size_t Read(void* buffer, size_t buffer_length) override { return ms_.read(buffer, buffer_length); }
-            [[nodiscard]] size_t GetTotalSampleCount() const override { return ms_.size() / format_.BlockAlign(); }
-            [[nodiscard]] size_t GetSampleCursor() const override { return ms_.tellg() / format_.BlockAlign(); }
-            size_t SetSampleCursor(size_t new_position) override { return ms_.seekg(new_position * format_.BlockAlign()) / format_.BlockAlign(); }
-            size_t Write(const void* buffer, size_t buffer_length) { return ms_.write(buffer, buffer_length); }
-        };
-
-        auto destination = std::make_shared<XtlMemStreamWaveSourceImpl>(input->GetFormat());
+        auto destination = AllocateWaveBuffer(input->GetFormat());
         auto temp_buffer = xtl::temp_memory_buffer();
         while (true)
         {
             constexpr size_t buf_size = 65536;
             void* p = temp_buffer.get(buf_size);
             size_t sz = sz = input->Read(p, buf_size);
-            (void)destination->Write(p, sz);
-            if (sz == 0) break;
+            (void)destination->Write(p, destination->Size(), sz);
+            if (sz == 0) break; // end
         }
 
         return destination;
+    }
+
+    std::shared_ptr<IRandomAccessWaveBuffer> DuplicateBuffer(std::shared_ptr<IRandomAccessWaveBuffer> source)
+    {
+        if (!source) return nullptr;
+
+        auto destination = AllocateWaveBuffer(source->GetFormat());
+
+        auto temp_buffer = xtl::temp_memory_buffer();
+        for (size_t i = 0; i < source->Size();)
+        {
+            constexpr size_t buf_size = 65536;
+            void* p = temp_buffer.get(buf_size);
+            size_t sz = sz = source->Read(p, i, buf_size);
+            (void)destination->Write(p, i, sz);
+            i += sz;
+            if (sz == 0) break; // ?
+        }
+
+        return destination;
+    }
+
+    std::shared_ptr<ISeekableWaveSource> AllocateReadCursor(std::shared_ptr<IRandomAccessWaveBuffer> buffer)
+    {
+        if (!buffer) return nullptr;
+
+        class WaveCursorImpl : public ISeekableWaveSource
+        {
+            std::shared_ptr<IRandomAccessWaveBuffer> target_{};
+            PcmWaveFormat format_{};
+            size_t cursor_{};
+
+        public:
+            WaveCursorImpl(std::shared_ptr<IRandomAccessWaveBuffer> target) : target_(std::move(target)), format_(target_->GetFormat()) {}
+
+            [[nodiscard]] PcmWaveFormat GetFormat() const override { return format_; }
+
+            [[nodiscard]] size_t Read(void* buffer, size_t buffer_length) override
+            {
+                size_t rd = target_->Read(buffer, cursor_, buffer_length);
+                cursor_ += rd;
+                return rd;
+            }
+
+            [[nodiscard]] size_t GetTotalSampleCount() const override { return target_->Size() / format_.BlockAlign(); }
+            [[nodiscard]] size_t GetSampleCursor() const override { return cursor_ / format_.BlockAlign(); }
+            size_t SetSampleCursor(size_t new_position) override { return cursor_ = std::min(new_position * format_.BlockAlign(), target_->Size()); }
+        };
+
+        return std::make_shared<WaveCursorImpl>(std::move(buffer));
     }
 }
