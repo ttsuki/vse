966369f913143357f07ef9a6b67c6453bcc249d3
ttsuki <ttsuki@ttsuki.dev>
2023-01-07 17:57:56 +0900

    Update pch.h

--- vse/pch.h
+++ vse/pch.h
@@ -1,5 +1,14 @@
+/// @file
+/// @brief  Vse - Pre-compiled header
+/// @author (C) 2022 ttsuki
+
 #pragma once
+
 #include <Windows.h>
 #include <combaseapi.h>
+#include <mmreg.h>
 
 #include "base/xtl/xtl_stdc++.h"
+
+#include "base/CommonTypes.h"
+#include "base/WaveFormat.h"
